aca2fba25448a07c45b9a98ae5e5fd1cf3ace783
Bryan Niwa <bryan@webbelectronicsinc.com>
2024-07-11 12:36:30 -0700

    feat(fish): provide option to clone git project to tmp

--- private_dot_config/private_fish/functions/tmp.fish
+++ private_dot_config/private_fish/functions/tmp.fish
@@ -2,4 +2,7 @@ function tmp
     [ "$argv" = view ] && cd /tmp/workspaces && cd $(ls -t | fzf --preview 'ls -A {}') && return 0
     set r "/tmp/workspaces/$(xxd -l3 -ps /dev/urandom)"
     mkdir -p -p "$r" && pushd "$r"
+    if [ $argv[1] ]
+        git clone $argv[1]
+    end
 end

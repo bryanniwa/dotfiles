39a80ca8d6c9702ccfd9e81e1ee39f16fc0f0e74
Bryan Niwa <bryan@webbelectronicsinc.com>
2024-10-08 11:47:29 -0700

    refactor(LazyVim): Simplify cp keymap

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "cp", "ciw<C-r>0<ESC>", { noremap = true }) -- replace the current word with the last yanked one
+vim.keymap.set("n", "cp", "viwP", { noremap = true }) -- replace the current word with the last yanked one
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
 -- Set ctrl+shift+v to paste from clipboard

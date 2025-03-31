bca0faecb6353ab046d3abb721e67c69adcb5881
Bryan Niwa <bryan@webbelectronicsinc.com>
2025-03-31 10:10:47 -0700

    Add noremap to custom keymaps and add comments

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,20 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "cp", "viwP", { noremap = true }) -- replace the current word with the last yanked one
+-- replace the current word with the last yanked one
+vim.keymap.set("n", "cp", "viwP", { noremap = true })
+
+-- Convenience mappings to exit insert mode
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+
 -- Set ctrl+shift+v to paste from clipboard
-vim.keymap.set({ "n", "v" }, "<C-S-v>", '"+P')
-vim.keymap.set("c", "<C-S-v>", "<C-R>+")
-vim.keymap.set("i", "<C-S-v>", '<ESC>l"+Pli')
+vim.keymap.set({ "n", "v" }, "<C-S-v>", '"+P', { noremap = true })
+vim.keymap.set("c", "<C-S-v>", "<C-R>+", { noremap = true })
+vim.keymap.set("i", "<C-S-v>", '<ESC>l"+Pli', { noremap = true })
+
+-- Convenience mappings for capitalization
+vim.keymap.set("n", "<M-u>", "gul", { noremap = true })
+vim.keymap.set("n", "<M-U>", "gUl", { noremap = true })
+vim.keymap.set("v", "<M-u>", "gu", { noremap = true })
+vim.keymap.set("v", "<M-U>", "gU", { noremap = true })
 
 vim.keymap.set("n", "<leader>ms", function()
   Snacks.terminal("spotify_player")
 end)
-
--- Improved upper/lower case motions
-vim.keymap.set("n", "<M-u>", "gul")
-vim.keymap.set("n", "<M-U>", "gUl")
-vim.keymap.set("v", "<M-u>", "gu")
-vim.keymap.set("v", "<M-U>", "gU")

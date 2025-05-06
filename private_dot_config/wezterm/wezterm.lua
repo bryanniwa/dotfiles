737fde377e5463dcaa340fe16b799077b39dc69d
Bryan Niwa <bryan@webbelectronicsinc.com>
2025-05-06 09:31:42 -0700

    Use Maple Mono and Catppuccin Mocha

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,10 +2,13 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font({
-	family = "Rec Mono Duotone",
+	-- family = "Rec Mono Duotone",
+	family = "Maple Mono NF",
+	harfbuzz_features = { "cv02=1", "cv35=1" },
 })
+config.warn_about_missing_glyphs = false
 config.enable_wayland = false
 config.font_size = 12
 config.switch_to_last_active_tab_when_closing_tab = true

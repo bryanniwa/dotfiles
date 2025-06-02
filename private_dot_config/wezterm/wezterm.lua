3822ad4fb2211f489ce344f4f47b3e82356489ba
Bryan Niwa <bryan@webbelectronicsinc.com>
2025-06-02 14:23:12 -0700

    Update wezterm config

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -6,11 +6,12 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font({
 	-- family = "Rec Mono Duotone",
 	family = "Maple Mono NF",
-	harfbuzz_features = { "cv02=1", "cv35=1" },
+	harfbuzz_features = { "cv02=1", "cv35=1", "cv64" },
 })
 config.warn_about_missing_glyphs = false
 config.enable_wayland = false
-config.font_size = 12
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.font_size = 11
 config.switch_to_last_active_tab_when_closing_tab = true
 
 config.keys = {
